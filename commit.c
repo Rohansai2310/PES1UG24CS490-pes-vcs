d815e703002d92fe30d4e02bca04b66357ff6b57
Rohan Sai <rohan.sai@example.com>
2026-04-20 17:31:37 +0530

    P4-C4: reject empty serialized commit payloads

--- commit.c
+++ commit.c
@@ -234,6 +234,11 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
     if (commit_serialize(&commit, &data, &len) != 0)
         return -1;
 
+    if (len == 0) {
+        free(data);
+        return -1;
+    }
+
     if (object_write(OBJ_COMMIT, data, len, commit_id_out) != 0)
     {
         free(data);
